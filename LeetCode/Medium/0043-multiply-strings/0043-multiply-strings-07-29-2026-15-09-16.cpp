723ceb674d101c9451c8c0433da3b567340851f7
Bipin Rawat <bsr29002@gmail.com>
2026-07-29 15:09:18 +0530

    0043-multiply-strings Medium .cpp Math 9.7 MB (52.13%) 3 ms (56.12%)

---
+++ LeetCode/Medium/0043-multiply-strings/0043-multiply-strings-07-29-2026-15-09-16.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    string multiply(string num1, string num2) {
+        int n1 = num1.size();
+        int n2 = num2.size();
+        if(num1 == "0" || num2 == "0")return "0";
+
+        vector<int>arr(n1+n2, 0);
+        //multiply every digit of num1 with evety digit of num2;
+        for(int i=n1-1; i>=0; i--){
+            for(int j=n2-1; j>=0; j--){
+
+                int product = (num1[i]-'0') * (num2[j] -'0');
+                int sum = product + arr[i+j+1];
+                // store the last digit
+                arr[i+j+1] = sum%10;
+                
+                // carrry goes to one position before 
+                arr[i+j] +=sum/10;
+
+            }
+        }
+        string ans = "";
+        int i=0;
+        while( i<arr.size() && arr[i] == 0)i++;
+
+        while( i< arr.size()){
+            ans+=arr[i]+'0';
+            i++;
+        }
+
+    return ans;
+
+    }
+};
