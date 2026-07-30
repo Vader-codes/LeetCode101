b992a99ffabd5ed32847f72e3a23273d1a92941e
Bipin Rawat <bsr29002@gmail.com>
2026-07-30 15:22:46 +0530

    0206-reverse-linked-list Easy .cpp Linked List 13.4 MB (72.87%) 0 ms (100%)

---
+++ LeetCode/Easy/0206-reverse-linked-list/0206-reverse-linked-list-07-30-2026-15-22-44.cpp
@@ -0,0 +1,31 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+    private:
+    ListNode* reverse_list(ListNode* head){
+        ListNode* prev = NULL;
+        ListNode* temp = head;
+        while(temp){
+            ListNode* next = temp->next;
+            temp->next = prev;
+            prev = temp;
+            temp = next;
+        }
+        return prev;
+    }
+public:
+    ListNode* reverseList(ListNode* head) {
+        if(!head || !head->next)return head;
+
+       return reverse_list(head);    
+  
+    }
+};
