e9dacfca816e647c65d4fa773ceed41be441657f
Bipin Rawat <bsr29002@gmail.com>
2026-07-31 16:13:41 +0530

    0127-word-ladder Hard .cpp Hash Table 21.3 MB (38.65%) 54 ms (67.17%)

---
+++ LeetCode/Hard/0127-word-ladder/0127-word-ladder-07-31-2026-16-13-39.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
+        // step 1 check if beginWord and endWord are the same
+        if(beginWord == endWord)return 1;
+
+
+        // step 2 : hash all the wordList into a set
+
+        unordered_set<string>st;
+        for(auto w : wordList)st.insert(w);
+        if(st.find(endWord) == st.end())return 0;
+
+        // step 3 we are doing a bfs for every combination from beginword to endwornd that are int wordList
+        queue<pair<int,string>>q;
+        q.push({1, beginWord});
+        int ans =0;
+        while(!q.empty()){
+            auto[step, word] = q.front();
+            q.pop();
+
+            if(word == endWord){
+                return step;
+            }
+
+            int n = word.length();
+            for(int i=0; i<n; i++){
+                char ch = word[i];
+
+                for(char c = 'a'; c<='z'; c++){
+                    word[i] = c;
+                    // we have to check for a cominaiton 
+                    if(st.find(word) != st.end()){
+                        q.push({step+1, word});
+                        // remove from the set
+                        st.erase(word);
+                    }         
+
+                }
+                
+                // change the back the word whih we changed
+                word[i] = ch;
+            }
+        }
+        return ans;
+    }
+};
