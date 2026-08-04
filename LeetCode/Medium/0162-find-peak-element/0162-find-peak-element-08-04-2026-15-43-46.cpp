72ed017941affe6acbf5c46454bf70c488f4c28a
Bipin Rawat <bsr29002@gmail.com>
2026-08-04 15:43:48 +0530

    0162-find-peak-element Medium .cpp Array 12.6 MB (41.44%) 0 ms (100%)

---
+++ LeetCode/Medium/0162-find-peak-element/0162-find-peak-element-08-04-2026-15-43-46.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int findPeakElement(vector<int>& nums) {
+        int n = nums.size();
+        int peak = -1;
+        if(n == 1)return 0;
+
+        for(int i=0; i<n; i++){
+            // middle element
+         if(i > 0 && i < n-1){
+            if(nums[i] > nums[i-1] && nums[i] > nums[i+1]) return i;
+         }
+        else if(i ==0){
+            if(nums[i] > nums[i+1]) return i;
+         }
+          else if(i == n-1 && nums[i] > nums[i-1])return i;
+        }
+        return -1;
+    }
+};
