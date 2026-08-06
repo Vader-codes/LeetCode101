1d1448eb689f3691575838ee33a86ce287946fe8
Bipin Rawat <bsr29002@gmail.com>
2026-08-06 18:03:12 +0530

    0373-find-k-pairs-with-smallest-sums Medium .cpp Array 177.6 MB (9.61%) 167 ms (10.61%)

---
+++ LeetCode/Medium/0373-find-k-pairs-with-smallest-sums/0373-find-k-pairs-with-smallest-sums-08-06-2026-18-03-09.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+    vector<vector<int>> kSmallestPairs(vector<int>& nums1, vector<int>& nums2, int k) {
+        typedef pair<int, vector<int>>p;
+
+        priority_queue<p, vector<p>, greater<p>>pq; // min heap {sum, {n1, n2}}
+        int n = nums1.size();
+        int m = nums2.size();
+
+        int sum = nums1[0] + nums2[0];
+        pq.push({sum, {0, 0}});
+        // we need to keep track of viisted indexes
+        set<pair<int,int>>st;
+        st.insert({0, 0});
+
+        vector<vector<int>>ans;
+
+        while( k-- && !pq.empty()){
+            auto temp  = pq.top(); // kth smallest pair
+            pq.pop();
+            int i = temp.second[0];
+            int j = temp.second[1];
+
+            // we can push this i , j pair to our anser
+            ans.push_back({nums1[i], nums2[j]});
+
+            // now we have to try next smallest which is either
+            // i, j+1, or i+1, j
+            if(j+1 < m && st.find({i, j+1}) == st.end()){
+                sum = nums1[i]+ nums2[j+1];
+                pq.push({sum, {i, j+1}});
+                st.insert({i, j+1});
+            }
+
+               if(i+1 < n && st.find({i+1, j}) == st.end()){
+                sum = nums1[i+1]+ nums2[j];
+                pq.push({sum, {i+1, j}});
+                st.insert({i+1, j});
+            }
+
+
+        }
+        return ans;
+    }
+};
+// Interview explanation
+
+// You can say:
+
+// "The algorithm performs at most k heap extractions because we stop after finding k pairs. Each extraction inserts at most two neighboring states. Both the priority queue and the visited set contain at most O(k) elements, so each heap or set operation takes O(log k). Therefore, the total time complexity is O(k log k), and the space complexity is O(k)."
