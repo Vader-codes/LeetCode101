54b6acbb96c5f113ad8c6a21da96b2f9feed6b5f
Bipin Rawat <bsr29002@gmail.com>
2026-08-11 13:09:26 +0530

    0654-maximum-binary-tree Medium .cpp Array 44 MB (30.84%) 16 ms (32.89%)

---
+++ LeetCode/Medium/0654-maximum-binary-tree/0654-maximum-binary-tree-08-11-2026-13-09-21.cpp
@@ -0,0 +1,48 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+    private:
+    int max_ele(vector<int>&nums, int left, int right){
+        int idx = -1;
+        int maxi = INT_MIN;
+        for(int i=left; i<= right; i++){
+            if(nums[i] > maxi ){
+                maxi = nums[i];
+                idx = i;
+            }
+        }
+        return idx;
+    }
+    TreeNode* helper(vector<int>&nums, int left, int right){
+        // base case for our recursion
+        if(left > right) return NULL;
+
+        //  we need to find the max element in the given range
+        int idx = max_ele(nums, left, right);
+        int val = nums[idx];
+       
+
+        // crate a node
+        TreeNode* root = new TreeNode(val);
+        // now for its left and right do the same recursively
+        root->left = helper(nums,left, idx-1 );
+        root->right = helper(nums, idx+1, right);
+        return root;
+
+    }
+public:
+    TreeNode* constructMaximumBinaryTree(vector<int>& nums) {
+       
+        return helper(nums, 0, nums.size()-1);
+
+    }
+};
