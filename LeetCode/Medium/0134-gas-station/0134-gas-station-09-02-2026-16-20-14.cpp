8ce29cd4093d4c134d53ec6fad53dad0acf6f766
Bipin Rawat <bsr29002@gmail.com>
2026-09-02 16:20:24 +0530

    0134-gas-station Medium .cpp Array 112.6 MB (8.55%) 0 ms (100%)

---
+++ LeetCode/Medium/0134-gas-station/0134-gas-station-09-02-2026-16-20-14.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
+        // figure our the toal gas and total cost of one circle
+        int n = gas.size();
+        int totalGas = accumulate(gas.begin(), gas.end(), 0);
+        int totalCost = accumulate(cost.begin(), cost.end(), 0);
+
+        if (totalGas < totalCost)
+            return -1; // we can never complete a circle
+
+        int idx = 0;
+        int tank = 0;
+
+        for (int i = 0; i < n; i++) {
+            tank += gas[i] - cost[i];
+
+            if (tank < 0) {
+                tank = 0;
+                idx = i + 1;
+            }
+        }
+        return idx;
+    }
+};
