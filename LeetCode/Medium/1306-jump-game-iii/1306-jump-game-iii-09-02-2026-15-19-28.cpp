a0247062d45f74d8ebdc554ab8f96e0be699b6d8
Bipin Rawat <bsr29002@gmail.com>
2026-09-02 15:19:38 +0530

    1306-jump-game-iii Medium .cpp Array 31.4 MB (66.64%) 0 ms (100%)

---
+++ LeetCode/Medium/1306-jump-game-iii/1306-jump-game-iii-09-02-2026-15-19-28.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+ bool solve(vector<int>&arr, int idx,  vector<int>&vis){
+    // base case
+    if(idx < 0  || idx >= arr.size())return false;
+    
+
+    if(vis[idx])return false;
+
+    if(arr[idx] == 0)return true;
+
+    vis[idx] = true;
+
+    // from this idx we can go idx+arr[idx], idx-arr[idx];
+
+    return solve(arr, idx+arr[idx], vis) || solve(arr, idx-arr[idx] , vis);
+ }
+    bool canReach(vector<int>& arr, int start) {
+        int n = arr.size();
+        vector<int>vis(n, false);
+        return solve(arr, start, vis);
+    }
+};
